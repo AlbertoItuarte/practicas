62b5b803be5826c325e1d091ef81a2da48604612
AlbertoItuarte <joaituarte@itparral.edu.mx>
2025-01-09 19:00:19 -0600

    Comentarios de los clientes

--- ParteBeto/src/App.jsx
+++ ParteBeto/src/App.jsx
@@ -1,6 +1,7 @@
 import Cards from "./components/Cards.jsx";
 import Features from "./components/Features.jsx";
 import Banner from "./components/Banner.jsx";
+import Comentarios from "./components/Comentarios.jsx";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Banner />
       <Features />
       <Cards />
+      <Comentarios />
     </div>
   );
 };
